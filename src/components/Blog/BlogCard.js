311653e420e61bbfb7e6ce772df5eee19f48f0ab
Aidil <aidilrozi83@gmail.com>
2021-01-09 19:50:37 -0600

    styled texts card

--- src/components/Blog/BlogCard.js
+++ src/components/Blog/BlogCard.js
@@ -1,16 +1,17 @@
 import { makeStyles } from "@material-ui/core/styles";
 import Grid from "@material-ui/core/Grid";
-import { Card, CardContent, CardMedia, Typography } from "@material-ui/core";
+import { CardContent, CardMedia, Typography } from "@material-ui/core";
 
 const useStyles = makeStyles((theme) => ({
   root: {
     flexGrow: 1,
-    overflowAnchor: "none",
   },
 
   media1: {
-    height: 680,
+    height: 648,
     backgroundPosition: "top",
+    display: "flex",
+    alignItems: "flex-end",
   },
   media2: {
     height: 480,
@@ -18,15 +19,33 @@ const useStyles = makeStyles((theme) => ({
   },
   paper1: {
     width: 300,
-    textAlign: "center",
-    color: theme.palette.text.secondary,
   },
   paper2: {
     backgroundSize: "cover",
     paddingLeft: 0,
     paddingRigth: 0,
-    textAlign: "center",
-    color: theme.palette.text.secondary,
+  },
+  cardContent: {
+    backgroundColor: "#f9f9f9",
+    textAlign: "left",
+    height: 150,
+  },
+  fontLarge: {
+    fontSize: "2.5rem",
+    fontFamily: "Luckiest Guy",
+    textTransform: "uppercase",
+    textAlign: "left",
+    color: "#fff",
+  },
+  font1: {
+    color: "#1db8f3",
+    fontWeight: "600",
+  },
+  font2: {
+    fontFamily: "Luckiest Guy",
+    textTransform: "uppercase",
+    fontSize: "1.6rem",
+    Color: "#000",
   },
 }));
 
@@ -42,29 +61,50 @@ export default function BlogCard() {
   return (
     <div className={classes.root}>
       <Grid container spacing={3}>
-        <Grid className={classes.paper1} item xs={12} md={6} lg={7} xl={6}>
-          <CardMedia className={classes.media1} image={gridImage[0]} />
+        <Grid className={classes.paper1} item xs={12} md={6} lg={7}>
+          <CardMedia className={classes.media1} image={gridImage[0]}>
+            <CardContent>
+              <Typography className={classes.fontLarge}>
+                Fortnite Crew: Green Arrow Revealed for January Crew P...
+              </Typography>
+            </CardContent>
+          </CardMedia>
         </Grid>
-        <Grid className={classes.paper1} item xs={12} md={6} lg={5} xl={6}>
-          <CardMedia className={classes.media1} image={gridImage[1]} />
+        <Grid className={classes.paper1} item xs={12} md={6} lg={5} xl={5}>
+          <CardMedia className={classes.media1} image={gridImage[1]}>
+            <CardContent>
+              <Typography className={classes.fontLarge}>
+                Marvel Royalty and Warriors Pack
+              </Typography>
+            </CardContent>
+          </CardMedia>
         </Grid>
         <Grid className={classes.paper2} item xs={12} md={6} lg={3} xl={3}>
           <CardMedia className={classes.media2} image={gridImage[2]} />
-          <CardContent style={{ backgrouncColor: "gray" }}>
-            <Typography gutterBottom variant="h5" component="h2">
-              Lizard
-            </Typography>
-            <Typography variant="body2" color="textSecondary" component="p">
-              Lizards are a widespread group of squamate reptiles, with over
-              6,000 species, ranging across all continents except Antarctica
+          <CardContent className={classes.cardContent}>
+            <Typography className={classes.font1}>Dec 21, 2020</Typography>
+            <Typography className={classes.font2}>
+              wakanda solute emote
             </Typography>
           </CardContent>
         </Grid>
         <Grid className={classes.paper2} item xs={12} md={6} lg={3} xl={3}>
           <CardMedia className={classes.media2} image={gridImage[3]} />
+          <CardContent className={classes.cardContent}>
+            <Typography className={classes.font1}>Dec 21, 2020</Typography>
+            <Typography className={classes.font2}>
+              fortography results
+            </Typography>
+          </CardContent>
         </Grid>
         <Grid className={classes.paper2} item xs={12} md={6} lg={3} xl={3}>
           <CardMedia className={classes.media2} image={gridImage[4]} />
+          <CardContent className={classes.cardContent}>
+            <Typography className={classes.font1}>Dec 18, 2020</Typography>
+            <Typography className={classes.font2}>
+              operation snowdown
+            </Typography>
+          </CardContent>
         </Grid>
       </Grid>
     </div>

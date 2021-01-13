8dac5222e3b14a6e0b260f142a70a82bf69e56f2
Aidil <aidilrozi83@gmail.com>
2021-01-13 04:09:19 -0600

    working on Footer content

--- src/components/Footer/Footer.elements.js
+++ src/components/Footer/Footer.elements.js
@@ -1,7 +1,67 @@
 import styled from "styled-components";
 
+const media = {
+  ipad: "@media only screen and (max-width: 768px)",
+};
+
 export const FooterContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
   height: 43vh;
   width: 100%;
   background-color: #2a2a2a;
+`;
+
+export const FooterInnerContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  height: 80%;
+  width: 90%;
+  background-color: #ff1;
+  color: #ccc;
+
+  ${media.ipad} {
+    flex-direction: column;
+  }
+`;
+
+export const FooterLeft = styled.div`
+  display: flex;
+  flex-direction: column;
+  flex: 1;
+  width: 50%;
+  height: 100%;
+  background-color: orange;
+
+  ul {
+    display: flex;
+    justify-content: space-around;
+    max-width: 140px;
+    font-size: 2.5rem;
+    background-color: yellow;
+  }
+
+  div {
+    display: flex;
+    flex-flow: wrap column;
+    height: 5vh;
+    width: 300px;
+    background-color: #dc143c;
+    font-size: 0.8rem;
+
+    a {
+      text-decoration: none;
+      color: #ccc;
+    }
+  }
+
+  ${media.ipad} {
+    width: 100%;
+  }
+`;
+export const FooterRight = styled.div`
+  width: 50%;
+  height: 100%;
+  background-color: #0e9;
 `;
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,9 +1,40 @@
-import { FooterContainer } from "./Footer.elements";
+import { YouTube } from "@material-ui/icons";
+import FacebookIcon from "@material-ui/icons/Facebook";
+import TwitterIcon from "@material-ui/icons/Twitter";
+import { FaSnapchat, FaTwitch } from "react-icons/fa";
+import {
+  FooterContainer,
+  FooterInnerContainer,
+  FooterLeft,
+  FooterRight,
+} from "./Footer.elements";
 
 const Footer = () => {
   return (
     <>
-      <FooterContainer></FooterContainer>
+      <FooterContainer>
+        <FooterInnerContainer>
+          <FooterLeft>
+            <ul>
+              <FacebookIcon />
+              <TwitterIcon />
+              <FaTwitch style={{ fontSize: "1.5rem", alignSelf: "center" }} />
+              <YouTube />
+            </ul>
+            <div>
+              <a href="">Home</a>
+              <a href="">News</a>
+              <a href="">FAQ</a>
+              <a href="">Help</a>
+              <a href="">Safety and Security</a>
+              <a href="">Competitive</a>
+              <a href="">Battle Pass</a>
+              <a href="">Get Fornite</a>
+            </div>
+          </FooterLeft>
+          <FooterRight></FooterRight>
+        </FooterInnerContainer>
+      </FooterContainer>
     </>
   );
 };

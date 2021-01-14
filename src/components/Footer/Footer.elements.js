daaa93e7746738779f95b948b9666be696a68aae
Aidil <aidilrozi83@gmail.com>
2021-01-14 15:38:05 -0600

    Footer component finished. But, still need some final touch

--- src/components/Footer/Footer.elements.js
+++ src/components/Footer/Footer.elements.js
@@ -1,27 +1,33 @@
 import styled from "styled-components/macro";
 
 const media = {
-  phone: "@media screen and (max-width: 376px)",
-  ipad: "@media screen and (max-width: 768px)",
+  phone320: "@media only screen and (max-width: 320px)",
+  phone375: "@media only screen and (min-width: 375px)",
+  ipad768: "@media only screen and (min-width: 768px)",
+  desktop1025: "@media only screen and (min-width: 1025px)",
+  tv1200: "@media only screen and (min-width: 1200px)",
 };
 
 export const Main = styled.footer`
   display: flex;
   justify-content: center;
   align-items: center;
-  height: 420px;
-  min-height: 420px;
+  min-height: calc(585px - 10vh);
   width: 100%;
   background-color: #2a2a2a;
+  overflow: hidden;
 
-  ${media.ipad} {
-    min-height: 87vh;
+  ${media.desktop1025} {
+    min-height: calc(885px - 25vh);
+  }
+  ${media.tv1200} {
+    min-height: calc(585px - 20vh);
   }
 `;
 export const MainContainer = styled.div`
   height: 90%;
   width: 90%;
-  background-color: #fafa;
+  /* background-color: #fafa; */
 `;
 export const SocialContainer = styled.div`
   display: flex;
@@ -29,8 +35,7 @@ export const SocialContainer = styled.div`
   justify-content: space-around;
   height: 50px;
   width: 180px;
-  background-color: #000;
-
+  /* background-color: #000; */
   ul {
     width: 170px;
     display: flex;
@@ -44,22 +49,71 @@ export const SocialContainer = styled.div`
 `;
 export const LinkContainer = styled.div`
   display: flex;
-  justify-content: space-between;
+  justify-content: center;
   flex-flow: wrap;
   width: 100%;
   height: 30%;
-  background-color: #f0f;
-
+  /* background-color: #f0f; */
+  div {
+    display: flex;
+    justify-content: center;
+    flex-flow: wrap;
+    height: 30%;
+  }
   ul {
+    display: flex;
+    justify-content: space-around;
+    flex-flow: column wrap;
+    width: 100%;
+    &:nth-child(1) {
+      flex: 1 1 20%;
+    }
+    &:nth-child(2) {
+      flex: 1 1 40%;
+    }
+    &:nth-child(3) {
+      flex: 1 1 30%;
+    }
     a {
-      font-size: 0.8rem;
+      font-size: 0.9rem;
       text-decoration: none;
       color: #ccc;
     }
   }
-
   img {
     align-self: center;
   }
+
+  ${media.ipad768} {
+    justify-content: space-between;
+    width: 100%;
+  }
 `;
-export const CopyrightContainer = styled.div``;
+export const CopyrightContainer = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  margin: 20px 0;
+  color: #ccc;
+
+  p {
+    width: 60%;
+    max-width: 70%;
+    height: 100%;
+  }
+`;
+export const PlateFormContainer = styled.div`
+  display: flex;
+  /* align-items: flex-end; */
+  width: 100%;
+  ul {
+    padding-top: 10px;
+    display: flex;
+    justify-content: space-evenly;
+    width: 100%;
+    color: #ccc;
+    .MuiSvgIcon-root {
+      font-size: 2rem;
+    }
+  }
+`;
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,7 +1,16 @@
 import ratedT from "../../images/text_image/ratedT.png";
-import { Instagram, YouTube } from "@material-ui/icons";
+import {
+  Android,
+  Games,
+  Instagram,
+  Phonelink,
+  Tv,
+  VideogameAsset,
+  YouTube,
+} from "@material-ui/icons";
 import FacebookIcon from "@material-ui/icons/Facebook";
 import TwitterIcon from "@material-ui/icons/Twitter";
+import SportsEsportsIcon from "@material-ui/icons/SportsEsports";
 import { FaTwitch } from "react-icons/fa";
 import {
   LinkContainer,
@@ -9,6 +18,7 @@ import {
   MainContainer,
   SocialContainer,
   CopyrightContainer,
+  PlateFormContainer,
 } from "./Footer.elements";
 
 const Footer = () => {
@@ -27,16 +37,23 @@ const Footer = () => {
           </ul>
         </SocialContainer>
         <LinkContainer>
-          <ul>
-            <a href="">Home</a>
-            <a href="">News</a>
-            <a href="">FAQ</a>
-            <a href="">Help</a>
-            <a href="">Safety and Security</a>
-            <a href="">Competitive</a>
-            <a href="">Battle Pass</a>
-            <a href="">Get Fornite</a>
-          </ul>
+          <div>
+            <ul>
+              <a href="">Home</a>
+              <a href="">News</a>
+              <a href="">FAQ</a>
+            </ul>
+            <ul>
+              <a href="">Help</a>
+              <a href="">Safety and Security</a>
+              <a href="">Competitive</a>
+            </ul>
+            <ul>
+              <a href="">Battle Pass</a>
+              <a href="">Get Fornite</a>
+              <a href="">Battle Pass</a>
+            </ul>
+          </div>
           <img src={ratedT} alt="rated" style={{ height: 60, width: 100 }} />
         </LinkContainer>
         <CopyrightContainer>
@@ -47,6 +64,15 @@ const Footer = () => {
             United States of America and elsewhere. All rights reserved.
           </p>
         </CopyrightContainer>
+        <PlateFormContainer>
+          <ul>
+            <SportsEsportsIcon />
+            <VideogameAsset />
+            <Games />
+            <Phonelink />
+            <Android />
+          </ul>
+        </PlateFormContainer>
       </MainContainer>
     </Main>
   );

2993e1911062cafc2edc8b2bb3f7efb76c65b9e7
Aidil <aidilrozi83@gmail.com>
2021-01-18 23:10:52 -0600

    Creating the skeleton and styling of mobile Merch component

--- src/components/Merch/Merch.elements.js
+++ src/components/Merch/Merch.elements.js
@@ -1,11 +1,22 @@
 import styled from "styled-components/macro";
 
 export const MerchMain = styled.div`
+  position: relative;
+  top: 51px;
   min-height: 100vh;
   min-width: 100%;
-  display: flex;
-  justify-content: center;
-  align-items: center;
   @media (max-width: 320px) {
   }
+`;
+
+export const MerchHeroContainer = styled.div`
+  min-height: 40vh;
+  width: 100%;
+  background-color: #f6e27f;
+`;
+
+export const MerchListCard = styled.div`
+  height: 100%;
+  margin: 0 20px;
+  background-color: #ccc;
 `;
--- src/components/Merch/Merch.js
+++ src/components/Merch/Merch.js
@@ -1,10 +1,24 @@
-import { MerchMain } from "./Merch.elements";
+import { MerchMain, MerchHeroContainer, MerchListCard } from "./Merch.elements";
 
 const Merch = () => {
   return (
     <>
       <MerchMain>
+        <MerchHeroContainer>
+          <h3>Hero</h3>
+        </MerchHeroContainer>
         <h1>Merch</h1>
+        <MerchListCard>
+          <h3>Item1</h3>
+          <h3>Item2</h3>
+          <h3>Item3</h3>
+          <h3>Item4</h3>
+          <h3>Item5</h3>
+          <h3>Item6</h3>
+          <h3>Item7</h3>
+          <h3>Item8</h3>
+          <h3>Item9</h3>
+        </MerchListCard>
       </MerchMain>
     </>
   );

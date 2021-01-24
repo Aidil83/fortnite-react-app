d75879d4ba6235a7580c18bf4d090e28f99993d4
Aidil <aidilrozi83@gmail.com>
2021-01-24 17:17:11 -0600

    finished with modal responsiveness. Still need to fix mobile landscape responsiveness

--- src/components/Modal/Modal.elements.js
+++ src/components/Modal/Modal.elements.js
@@ -4,10 +4,10 @@ import CloseIcon from '@material-ui/icons/Close';
 
 export const ModalMain = styled.div`
   position: fixed;
-  top: 25px;
+  top: 95px;
   z-index: 1;
   width: 100%;
-  height: 90vh;
+  height: 80vh;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -21,10 +21,15 @@ export const ModalMain = styled.div`
     justify-content: flex-end;
     height: 100%;
     max-height: 500px;
-    width: 100%;
-    width: min(90vw, 800px);
+    min-width: 300px;
+    width: 80%;
+    max-width: 400px;
+    /* width: 100%; */
     border-radius: 24px;
     box-shadow: 0px 0px 14px 3px rgba(0, 0, 0, 0.4);
+    @media screen and (min-width: 450px) {
+      max-height: 650px;
+    }
     .MuiSvgIcon-root {
         font-size: 2rem;
     }
@@ -34,13 +39,15 @@ export const ModalMain = styled.div`
       height: 100%;
       width: 100%;
       background: ${ModalData.legendCard.gradOrange};
-      -webkit-clip-path:circle(320px at 50% 60vh);
-      clip-path: circle(310px at 50% -8vh);
+      -webkit-clip-path:circle(85% at 50% -15%);
+      clip-path: circle(85% at 50% -15%);
     }
   }
 `;
 
 export const ModalClose = styled(CloseIcon)`
+  position: absolute;
+  z-index: 1;
   height: 30px;
   margin: 13px;
   color: #FFAE35;
@@ -50,8 +57,10 @@ export const InfoContainer = styled.div`
   position: absolute;
   bottom: 0;
   z-index: 0;
-  height: 272px;
-  width: inherit;
+  height: 100%;
+  max-height: 572px;
+  min-width: 300px;
+  width: 100%;
   display: flex;
   align-items: flex-end;
   background-color: white;

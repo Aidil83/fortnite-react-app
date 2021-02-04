8e4589671b6ab7d57caca59545f336dc2283192e
Aidil <aidilrozi83@gmail.com>
2021-02-03 18:42:34 -0600

    Finished writing out the html skeleton for cart component.

--- src/components/Cart/Cart.elements.js
+++ src/components/Cart/Cart.elements.js
@@ -5,12 +5,29 @@ export const Main = styled.div`
   width: 100%;
   display: flex;
   justify-content: center;
+  overflow: hidden;
+  background-color: #ccc;
 `;
 export const MainContainer = styled.div`
   height: 100vh;
-  width: 85%;
-  background-color: #ffd587;
-  div {
-    
-  }
+  width: 90%;
+  position: relative;
+  top: 51px;
+  background-color: #fff;
+
 `;
+
+export const Title = styled.div``;
+
+export const Info = styled.div``;
+export const Info__header = styled.div``;
+export const Info__content = styled.div``;
+
+export const Cart__line = styled.hr``;
+
+export const Total = styled.div``;
+export const Total__header = styled.div``;
+export const Total__subtotal = styled.div``;
+export const Total__shipping = styled.div``;
+export const Total__total = styled.div``;
+export const Total__btn = styled.div``;
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,9 +1,55 @@
-import {Main, MainContainer} from './Cart.elements'
+import {Main, MainContainer, Title, Cart__line} from './Cart.elements'
+import {Info, Info__content, Info__header, Total, Total__header, Total__subtotal} from './Cart.elements'
+import {Total__shipping, Total__total, Total__btn} from './Cart.elements'
 
 const Cart = () => {
   return (
     <Main>
-      <MainContainer>Cart</MainContainer>
+      <MainContainer>
+        <Title><h1>my cart</h1></Title>
+        <Info>
+          <Info__header>
+            <h2 className="header-label">Product</h2>
+            <h2 className="header-label">Price</h2>
+            <i className="edit-icon"></i>
+          </Info__header>
+          <Cart__line />
+          <Info__content>
+            <div className="image-container">
+              <img src="" alt="" className="image-item" />
+            </div>
+            <div className="name-container">
+              <h3 className="product-name">Omega</h3>
+              <h4 className="product-rarity">legendary</h4>
+            </div>
+            <h3 className="product-price">$5</h3>
+            <i className="close-icon"></i>
+            <hr className="product-underline" />
+          </Info__content>
+        </Info>
+        <Cart__line />
+        {/* Cart card */}
+        <Total>
+          <Total__header>cart totals</Total__header>
+          <Total__subtotal>
+            <div className="subtotal-name">Subtotal</div>
+            <div className="subtotal-price">$99.99</div>
+          </Total__subtotal>
+          <Cart__line />
+          <Total__shipping>
+            <div className="shipping-name">Shipping</div>
+            <div className="shipping-price">Free</div>
+          </Total__shipping>
+          <Cart__line />
+          <Total__total>
+            <div className="total-name">Total</div>
+            <div className="total-price">$99.99</div>
+          </Total__total>
+        </Total>
+        <Total__btn>
+          <h2>proceed to checkout</h2>
+        </Total__btn>
+      </MainContainer>
     </Main>
   )
 }

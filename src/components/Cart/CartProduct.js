f96ae0dd0b86e1edd5aa821215148681c44f8a87
Aidil <aidilrozi83@gmail.com>
2021-02-09 16:04:23 -0600

    Fixed bug that removed 2 products by removing isClose useState.

--- src/components/Cart/CartProduct.js
+++ src/components/Cart/CartProduct.js
@@ -9,37 +9,33 @@ import {
 } from "./Cart.elements";
 
 const CartProduct = ({ purchasedItem }) => {
-  const [isClose, setIsClose] = useState(false);
   const { modalData, modalPrice } = purchasedItem;
   // double destructuring:
   const [, dispatch] = useContext(StateContext);
 
-  const handleClose = (productId) => {
-    setIsClose(true);
+  const handleClose = () => {
     dispatch({
       type: "REMOVE_FROM_CART",
-      payload: productId,
+      payload: modalData.id,
     });
   };
 
   return (
     <>
-      {!isClose && (
-        <Info>
-          <Info__productContainer>
-            <Info__product>
-              <img className="product-image" src={modalData.images.smallIcon} />
-            </Info__product>
-            <Info__name>
-              <div className="product-name">{modalData.name}</div>
-              <div className="product-rarity">{modalData.rarity.value}</div>
-            </Info__name>
-            <div className="price-cost">${modalPrice.price}</div>
-            <Info__close onClick={() => handleClose(modalData.id)} />
-          </Info__productContainer>
-          <hr style={{ opacity: 0.3 }} />
-        </Info>
-      )}
+      <Info>
+        <Info__productContainer>
+          <Info__product>
+            <img className="product-image" src={modalData.images.smallIcon} />
+          </Info__product>
+          <Info__name>
+            <div className="product-name">{modalData.name}</div>
+            <div className="product-rarity">{modalData.rarity.value}</div>
+          </Info__name>
+          <div className="price-cost">${modalPrice.price}</div>
+          <Info__close onClick={handleClose} />
+        </Info__productContainer>
+        <hr style={{ opacity: 0.3 }} />
+      </Info>
     </>
   );
 };

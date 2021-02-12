4a67a4c974e338c254f85a41417f828bfcfe3b8c
Aidil <aidilrozi83@gmail.com>
2021-02-12 14:19:07 -0600

    Gave background-image for checkout page as a template. Working on cart indication when user click purchase.

---
+++ src/components/Checkout/Checkout.elements.js
@@ -0,0 +1,10 @@
+import styled from "styled-components/macro";
+
+export const Main = styled.div`
+  width: 100%;
+  height: 100vh;
+  img {
+    width: 100%;
+    height: 100vh;
+  }
+`;
--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,5 +1,11 @@
+import { Main } from "./Checkout.elements";
+import checkout from "../../images/checkout/checkout-idea.jpg";
 const Checkout = () => {
-  return <>Checkout</>;
+  return (
+    <Main>
+      <img src={checkout} alt="" />
+    </Main>
+  );
 };
 
 export default Checkout;

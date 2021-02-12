6b9062e424d64fdfa2f692669a2837c9e09ba9c2
Aidil <aidilrozi83@gmail.com>
2021-02-11 23:50:23 -0600

    Added cart link for sidebar. Will create another link to cart page for easier navigation.

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -1,16 +1,31 @@
-import {SidebarItem, SidebarMain, SidebarMenu} from "./Sidebar.elements";
+import { SidebarItem, SidebarMain, SidebarMenu } from "./Sidebar.elements";
 
-const Sidebar = ({isOpen, handleToggle}) => {
+const Sidebar = ({ isOpen, handleToggle }) => {
   return (
     <>
       <SidebarMain isOpen={isOpen}>
         <SidebarMenu>
-          <SidebarItem to="/battle_Pass" onClick={handleToggle}>BATTLE PASS</SidebarItem>
-          <SidebarItem to="/creative" onClick={handleToggle}>CREATIVE</SidebarItem>
-          <SidebarItem to="/save_the_world" onClick={handleToggle}>SAVE THE WORLD</SidebarItem>
-          <SidebarItem to="/competitive" onClick={handleToggle}>COMPETIVE</SidebarItem>
-          <SidebarItem to="/news" onClick={handleToggle}>NEWS</SidebarItem>
-          <SidebarItem to="/merch" onClick={handleToggle}>MERCH</SidebarItem>
+          <SidebarItem to="/battle_Pass" onClick={handleToggle}>
+            BATTLE PASS
+          </SidebarItem>
+          <SidebarItem to="/creative" onClick={handleToggle}>
+            CREATIVE
+          </SidebarItem>
+          <SidebarItem to="/save_the_world" onClick={handleToggle}>
+            SAVE THE WORLD
+          </SidebarItem>
+          <SidebarItem to="/competitive" onClick={handleToggle}>
+            COMPETIVE
+          </SidebarItem>
+          <SidebarItem to="/news" onClick={handleToggle}>
+            NEWS
+          </SidebarItem>
+          <SidebarItem to="/merch" onClick={handleToggle}>
+            MERCH
+          </SidebarItem>
+          <SidebarItem to="/cart" onClick={handleToggle}>
+            CART
+          </SidebarItem>
         </SidebarMenu>
       </SidebarMain>
     </>

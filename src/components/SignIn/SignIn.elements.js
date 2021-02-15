0ddf1e748cb49b4e559a74f5a2bbb3e19a680b21
Aidil <aidilrozi83@gmail.com>
2021-02-14 18:55:40 -0600

    Finished writing out the html signIn content. Working on styling it.

--- src/components/SignIn/SignIn.elements.js
+++ src/components/SignIn/SignIn.elements.js
@@ -2,6 +2,40 @@ import styled from "styled-components/macro";
 
 export const Main = styled.div`
   width: 100%;
-  height: 100vh;
-  background-color: orange;
+  min-height: 100vh;
+  margin-top: 51px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
+
+export const LoginContainer = styled.div`
+  width: 85%;
+  min-height: 80vh;
+  height: 100%;
+  margin-bottom: 20%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: lime;
+`;
+
+export const LoginInner = styled.div`
+  width: 90%;
+  min-height: 430px;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  & .login-name {
+    margin-bottom: 1em;
+  }
+  & .username {
+    margin-bottom: 0.2em;
+  }
+  & input {
+    height: 2rem;
+    border: none;
+    border-radius: 4px;
+  }
 `;
--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,6 +1,21 @@
-import { Main } from "./SignIn.elements";
+import { LoginContainer, LoginInner, Main } from "./SignIn.elements";
 const SignIn = () => {
-  return <Main>Sign In</Main>;
+  return (
+    <Main>
+      <LoginContainer>
+        <LoginInner>
+          <div className="login-name">Login</div>
+          <div className="username">Username</div>
+          <input type="text" />
+          <div className="password">Password</div>
+          <input type="text" />
+          <button className="login-btn">login</button>
+          <span className="option-title">or login with</span>
+          <button className="login-demo">Demo</button>
+        </LoginInner>
+      </LoginContainer>
+    </Main>
+  );
 };
 
 export default SignIn;

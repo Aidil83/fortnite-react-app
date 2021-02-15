498d099132629fe29c782710b95e1ca75b3e8cd8
Aidil <aidilrozi83@gmail.com>
2021-02-14 20:49:22 -0600

    Finished styling sign in content.

--- src/components/SignIn/SignIn.elements.js
+++ src/components/SignIn/SignIn.elements.js
@@ -18,7 +18,8 @@ export const LoginContainer = styled.div`
   flex-direction: column;
   align-items: center;
   justify-content: center;
-  background-color: lime;
+  background-color: #e7e7e7;
+  border-radius: 4px;
 `;
 
 export const LoginInner = styled.div`
@@ -29,13 +30,78 @@ export const LoginInner = styled.div`
   flex-direction: column;
   & .login-name {
     margin-bottom: 1em;
+    font-family: Roboto;
+    font-weight: bold;
+    font-size: 24px;
+    line-height: 28px;
+    margin-left: 0.15em;
   }
-  & .username {
-    margin-bottom: 0.2em;
+  & .login-username {
+    margin-bottom: 0.3em;
+    font-family: Roboto;
+    font-weight: normal;
+    font-size: 18px;
+    line-height: 21px;
+    margin-left: 0.25em;
+  }
+  & .login-password {
+    margin-top: 1.75em;
+    margin-bottom: 0.3em;
+    margin-left: 0.25em;
   }
   & input {
-    height: 2rem;
+    height: 2.2rem;
     border: none;
-    border-radius: 4px;
+    border-radius: 10px;
+  }
+  & .login-btn {
+    margin: 3.5em auto;
+    width: fit-content;
+    padding: 1em 7em;
+    background-color: #8000ff;
+    color: #fff;
+    border: none;
+    border-radius: 20px;
+    font-family: Roboto;
+    text-transform: uppercase;
+    font-size: 10px;
+    line-height: 12px;
+  }
+  & .option-title {
+    display: flex;
+    width: 100%;
+    align-self: center;
+    align-items: center;
+    font-family: Roboto;
+    font-size: 12px;
+    line-height: 14px;
+    margin: 0.8em 0;
+    color: #a6a6a6;
+    & div {
+      width: 200px;
+      text-align: center;
+    }
+    & hr {
+      width: 80%;
+      border: none;
+      height: 2px;
+      background: #fff;
+    }
+  }
+  & .login-demo {
+    align-self: center;
+    display: flex;
+    align-items: center;
+    margin-top: 2em;
+    padding: 0.5em 1.8em;
+    background: #e0ffe9;
+    border-radius: 20px;
+    border: 1px solid #a9a9a9;
+    color: #6d6d6d;
+    & .demo-logo {
+      margin-right: 0.2em;
+      font-size: 1.4rem;
+      color: #00ff00;
+    }
   }
 `;
--- src/components/SignIn/SignIn.js
+++ src/components/SignIn/SignIn.js
@@ -1,17 +1,25 @@
 import { LoginContainer, LoginInner, Main } from "./SignIn.elements";
+import { HiBadgeCheck } from "react-icons/hi";
 const SignIn = () => {
   return (
     <Main>
       <LoginContainer>
         <LoginInner>
           <div className="login-name">Login</div>
-          <div className="username">Username</div>
+          <div className="login-username">Username</div>
           <input type="text" />
-          <div className="password">Password</div>
+          <div className="login-password">Password</div>
           <input type="text" />
           <button className="login-btn">login</button>
-          <span className="option-title">or login with</span>
-          <button className="login-demo">Demo</button>
+          <div className="option-title">
+            <hr />
+            <div>or login with</div>
+            <hr />
+          </div>
+          <button className="login-demo">
+            <HiBadgeCheck className="demo-logo" />
+            <div className="demo-name">Demo</div>
+          </button>
         </LoginInner>
       </LoginContainer>
     </Main>

e123f7e3ccf5e08911f9ce38f999aed415a24ac3
Aidil <aidilrozi83@gmail.com>
2021-02-21 12:51:29 -0600

    Removed all horrible media quries.

--- src/components/Wrapper/Wrapper.elements.js
+++ src/components/Wrapper/Wrapper.elements.js
@@ -31,7 +31,6 @@ export const WrapperImage = styled.div`
 
   /* NOTE: Prevents auto scrolling after refreshing page: */
   overflow-anchor: none;
-
   &.mobile2 {
     @media (max-width: 768px) {
       background-image: url(${({ imgMobile }) => imgMobile});
@@ -101,21 +100,6 @@ export const NextBtn = styled.button`
   &:hover {
     background-color: orange;
   }
-
-  ${media.mobile} {
-    bottom: 113px;
-    right: 6px;
-    height: 60px;
-    width: 60px;
-  }
-
-  ${media.mobileSm} {
-    bottom: 123px;
-    right: 3px;
-    height: 40px;
-    width: 40px;
-    font-size: 1rem;
-  }
 `;
 
 export const WrapperTextContainer = styled.div`
@@ -128,13 +112,6 @@ export const WrapperTextContainer = styled.div`
   position: absolute;
   left: ${({ slide }) => slide.WrapperContainerLeft};
   bottom: ${({ slide }) => slide.WrapperContainerBottom};
-
-  @media only screen and (min-width: 768px) {
-    height: ${({ slide }) => slide.WrapperTextContainerHeight};
-    width: 100%;
-    bottom: 0;
-    left: 0px;
-  }
 `;
 
 export const WrapperCaption = styled.div`
@@ -144,10 +121,6 @@ export const WrapperCaption = styled.div`
   font-family: "Luckiest Guy", cursive;
   font-size: clamp(2rem, 9.6vw, 2.3rem);
   color: ${({ slide }) => slide.captionColor};
-
-  ${media.ipad} {
-    height: ${({ slide }) => slide.posCaptionHeight};
-  }
 `;
 
 export const WrapperTitleTxt = styled.div`
@@ -166,39 +139,6 @@ export const WrapperTitleTxt = styled.div`
   /* NOTE: Ignore the linting: */
   -webkit-background-clip: text;
   -webkit-text-fill-color: transparent;
-
-  ${media.Laptop} {
-    margin-right: ${({ slide }) => slide.marginRightTitleLaptop}px;
-  }
-
-  @media screen and (max-width: 1190px) {
-    width: ${({ slide }) => slide.widthTitleLaptop}px;
-    right: inherit;
-    margin-right: ${({ slide }) => slide.marginRightTitleLaptopSm}px;
-    font-size: ${({ slide }) => slide.fontSizeTitleIpadSm};
-  }
-
-  ${media.ipadPro} {
-    bottom: ${({ slide }) => slide.posTitleBottomIpadPro}px;
-    line-height: 80.4px;
-    font-size: 90px;
-  }
-
-  @media only screen and (max-width: 768px) {
-    margin-right: 0;
-    width: ${({ slide }) => slide.widthTitleIpad}px;
-    font-size: ${({ slide }) => slide.fontSizeTitleIpadSm};
-    right: ${({ slide }) => slide.posTitleRightIpad}px;
-    bottom: ${({ slide }) => slide.posTitleBottomMobileWeb};
-  }
-
-  ${media.ipad} {
-    font-size: ${({ slide }) => slide.fontSizeTitleIpad};
-    line-height: 80.4px;
-    align-self: center;
-    width: 100%;
-    height: ${({ slide }) => slide.wrapperTxtHeight};
-  }
 `;
 
 export const WrapperText = styled.img`
@@ -225,52 +165,17 @@ export const WrapperText = styled.img`
       margin-bottom: -45px;
     }
   }
-
-  @media only screen and (max-width: 620px) {
-    width: ${({ slide }) => slide.imgmobileBigwidth};
-    height: ${({ slide }) => slide.imgmobileBigheight};
-    left: 0;
-
-    &.mobile3 {
-      height: 100px;
-      bottom: 156%;
-      margin-bottom: -55px;
-    }
-  }
-
-  @media only screen and (max-width: 320px) {
-    width: ${({ slide }) => slide.imgmobileBigwidth};
-    height: ${({ slide }) => slide.imgmobileBigheight};
-    left: 0;
-  }
 `;
 
 export const WrapperSection = styled.div`
   display: flex;
   justify-content: center;
-  /* position: absolute; */
   align-self: center;
   font-family: "Luckiest Guy", cursive;
   font-size: 2.2rem;
   width: 200px;
   height: 45px;
   left: 297px;
-
-  ${media.Laptop} {
-    left: 297px;
-    font-size: 2rem;
-  }
-
-  ${media.ipadPro} {
-    left: 300px;
-  }
-
-  ${media.ipad} {
-    height: 12%;
-    position: absolute;
-    left: auto;
-    bottom: 8%;
-  }
 `;
 
 // NOTE: Animate after refresh page:
@@ -312,8 +217,6 @@ export const WrapperLearnMore = styled.p`
   color: black;
   font-family: "Luckiest Guy", cursive;
   font-size: 1.5rem;
-  /* width: 230px;
-  height: 100%; */
   letter-spacing: 2.1px;
   transition: 550ms ease;
   color: ${({ slide }) => slide.txtBtnColor};

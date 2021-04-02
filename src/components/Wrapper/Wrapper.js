8c94fb19e1e2305852914d32cf4d736edce2f09b
Aidil <aidilrozi83@gmail.com>
2021-04-02 12:53:28 -0500

    Re-enabled auto sliders.

--- src/components/Wrapper/Wrapper.js
+++ src/components/Wrapper/Wrapper.js
@@ -34,7 +34,7 @@ const Wrapper = ({ slides }) => {
       setCurrent((current) => (current === length - 1 ? 0 : current + 1));
     };
 
-    // timeout.current = setTimeout(nextSlide, 4000);
+    timeout.current = setTimeout(nextSlide, 4000);
 
     return function () {
       if (timeout.current) clearTimeout(timeout.current);
